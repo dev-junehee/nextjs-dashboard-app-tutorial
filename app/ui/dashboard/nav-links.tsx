c1fd6bb24acb1c7605c56fde9d1feb25de2c9452
junehee <dev.junehee@gmail.com>
2023-11-22 14:56:51 +0900

    feat: 클라이언트 컴포넌트로 변경, usePathname에 따라 클래스명 변경 (clsx 사용)

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,11 +1,18 @@
+'use client';
+// usePathname 훅은 현재 URL의 경로 이름을 읽을 수 있는 클라이언트 구성 요소이기 때문에
+// use client 선언을 통해 의도적으로 클라이언트 컴포넌트로 사용해야 한다.
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import clsx from 'clsx';
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
+// 측면 탐색에 표시할 링크의 지도입니다.
+// 응용 프로그램의 크기에 따라 데이터베이스에 저장됩니다.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
@@ -17,19 +24,26 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'lex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
